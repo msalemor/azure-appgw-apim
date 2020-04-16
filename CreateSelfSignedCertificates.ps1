bed57d003b26b9fb0d96e19d7db9abcce379abdc
Alex Morales <44649358+msalemor@users.noreply.github.com>
2020-04-16 14:12:20 -0400

    Update CreateSelfSignedCertificates.ps1

--- CreateSelfSignedCertificates.ps1
+++ CreateSelfSignedCertificates.ps1
@@ -1,9 +1,11 @@
+# Settings
 $domain = "yourdomain.com"
 $gatewayDomain = "api.$domain"
 $portalDomain = "portal.$domain"
 $path = "c:\certs"
 $certPwd="P@tojit0"
 
+# Create signing certiface
 $params = @{
   DnsName = $domain
   KeyLength = 2048
@@ -18,6 +20,7 @@ $rootCA = New-SelfSignedCertificate @params
 Export-Certificate -Cert $rootCA -FilePath "$path\rootCA.cer"
 Import-Certificate -CertStoreLocation 'Cert:\LocalMachine\Root' -FilePath "$path\rootCA.cer"
 
+# create the gateway certificate signed by the root CA
 $params = @{
   DnsName = $gatewayDomain
   Signer = $rootCA
@@ -29,9 +32,11 @@ $params = @{
   CertStoreLocation = 'Cert:\LocalMachine\My'
 }
 $gatewayCert = New-SelfSignedCertificate @params
+# Export the gateway certificate
 Export-Certificate -Cert $gatewayCert -FilePath "$path\$gatewayDomain.cer"
 Export-PfxCertificate -Cert $gatewayCert -FilePath "$path\$gatewayDomain.pfx" -Password (ConvertTo-SecureString -AsPlainText $certPwd -Force)
 
+# create the portal certificate signed by the root CA
 $params = @{
   DnsName = $portalDomain
   Signer = $rootCA
@@ -43,6 +48,7 @@ $params = @{
   CertStoreLocation = 'Cert:\LocalMachine\My'
 }
 $portalCert = New-SelfSignedCertificate @params
+# Export the portal certificate
 Export-Certificate -Cert $portalCert -FilePath "$path\$portalDomain.cer"
 Export-PfxCertificate -Cert $portalCert -FilePath "$path\$portalDomain.pfx" -Password (ConvertTo-SecureString -AsPlainText $certPwd -Force)
 
