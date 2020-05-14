8630e11f5d8f3bd6a58e60720687a9cfc19d5a88
Alex Morales <44649358+msalemor@users.noreply.github.com>
2020-05-14 13:01:51 -0400

    Update DeployPrivateZone.ps1

--- DeployPrivateZone.ps1
+++ DeployPrivateZone.ps1
@@ -1,7 +1,15 @@
-## Deploy a private DNS Zone
+## Settings
+$domain = "yourdomain.com"
+$vnetID = "<--The VNET ID from the subscription-->"
+$name = "myNetworkLink"
+
+## Load the private zone module
 Install-Module -Name Az.PrivateDns -force
 
+## Create a Zone
 $zone = New-AzPrivateDnsZone -Name private.$domain -ResourceGroupName $resGroupName
+
+## Connects the network to the private zone
 $link = New-AzPrivateDnsVirtualNetworkLink -ZoneName private.$domain `
-  -ResourceGroupName $resGroupName -Name "mylink" `
-  -VirtualNetworkId $vnet.id -EnableRegistration
+  -ResourceGroupName $resGroupName -Name $name `
+  -VirtualNetworkId $vnetID -EnableRegistration
